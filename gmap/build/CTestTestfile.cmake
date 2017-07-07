0cc166aaf3e4d9fde810e14cb009a2ebabffd276
pawel <pwegrzyn54@gmail.com>
2017-07-07 13:25:35 +0200

    xd wow

--- gmap/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pawel/gmap/src
-# Build directory: /home/pawel/gmap/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(slam_gmapping/slam_gmapping)
-subdirs(slam_gmapping/gmapping)
--- gmap/build/slam_gmapping/slam_gmapping/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pawel/gmap/src/slam_gmapping/slam_gmapping
-# Build directory: /home/pawel/gmap/build/slam_gmapping/slam_gmapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
